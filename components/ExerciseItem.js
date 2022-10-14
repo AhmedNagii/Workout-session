6f1a174f844b40b557ee248e33173a560c4d7e18
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-10-14 22:23:42 +0200

    conflict between add func and edit func

--- components/ExerciseItem.js
+++ components/ExerciseItem.js
@@ -1,10 +1,11 @@
-export default function ExerciseItem(itemName , repetitions ,id){
-    return `
-    <li id="${id}" class="exercise-item">
-    <h2>${itemName} * ${repetitions}</h2>
+export default function ExerciseItem(item) {
+  return `
+    <li id="${item.id}" class="exercise-item">
+    <h2>${item.exerciseName} * ${item.exerciseRepetitions}</h2>
     <span>
-      <img src="assets/edit.png" class="edit-icon" />
-      <img src="assets/delete.png" class="delete-icon" />
+      <img  src="assets/edit.png"    class="edit-icon" data-id=${item.id} />
+      <img  src="assets/delete.png" class="delete-icon" data-id=${item.id} />
     </span>
   </li>`
 }
+
--- main.js
+++ main.js
@@ -7,52 +7,102 @@ const popUpContainer = document.querySelector(".popUp-container")
 const repetitionsInput = document.getElementById('repetitions')
 const exerciseNameInput = document.getElementById('exercise_name')
 const exerciseList = document.querySelector('.exercise-list')
+const addItemBtn = document.querySelector(".add-item-btn")
+const deleteIcon = document.querySelector('.delete-icon')
+
+
+let itemsArr = JSON.parse(localStorage.getItem('items')) || [];
 
 
 
-let itemsArr = []
 
+function addItem() {
+    itemsArr.push({
+        exerciseName: exerciseNameInput.value,
+        exerciseRepetitions: repetitionsInput.value,
+        id: Math.floor(Math.random() * 100)
+    })
+    exerciseNameInput.value = ""
+    repetitionsInput.value = ""
+    popUpContainer.classList.remove('show')
+    renderItems()
 
-function addItem (){
+}
 
- 
-    exerciseList.innerHTML += ExerciseItem(exerciseNameInput.value, repetitionsInput.value)
-        itemsArr.push({
-            exerciseName:  exerciseNameInput.value,
-            exerciseRepetitions:repetitionsInput.value,
-            id: itemsArr.length * 3
-        }) 
-        exerciseNameInput.value = ""
-        repetitionsInput.value = ""
-        popUpContainer.classList.remove('show')
-      console.log(exerciseList)
-        
-   
+function deleteItem() {
+    document.body.addEventListener('click', (event) => {
+        event.stopPropagation(); //prevent bubbling
+        if (!event.target.matches('.delete-icon')) return;
+        const id = event.target.dataset.id;
+        console.log(id)
+        itemsArr = itemsArr.filter(item => item.id !== Number(id));
+        renderItems();
+    })
+
 }
 
 
-popUpAddBtn.addEventListener('click' , (e) => {
-    e.preventDefault()
-    addItem()
-   
-})
+function editeItem() {
+    document.body.addEventListener('click', (event) => {
+        event.stopPropagation(); //prevent bubbling
+        if (!event.target.matches('.edit-icon')) return;
+        const id = event.target.dataset.id;
+        const selectItem = itemsArr.filter(item => id.slice(0, 2) == item.id);
 
-// function deleteItem (){
-  
-// }
+        exerciseNameInput.value = selectItem[0].exerciseName
+        repetitionsInput.value = Number(selectItem[0].exerciseRepetitions)
+        popUpContainer.classList.add('show')
 
-document.querySelector(".add-item-btn").addEventListener('click',() => {
+        itemsArr.map(item => {
+            item.id === Number(id) ? { ...item, exerciseNameInput, repetitionsInput } : item
+        })
+
+    })
+}
+
+
+addItemBtn.addEventListener('click', () => {
     popUpContainer.classList.add('show')
 })
 
-
-
-popUpCancelBtn.addEventListener('click' , (e) => {
+popUpAddBtn.addEventListener('click', (e) => {
     e.preventDefault()
-    
+    addItem()
+})
+
+popUpCancelBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    exerciseNameInput.value = ""
+    repetitionsInput.value = ""
     popUpContainer.classList.remove('show')
 })
 
 
 
 
+
+
+
+
+
+function addListerns() {
+    deleteItem()
+    editeItem()
+}
+
+
+function renderItems() {
+    displayItems();
+    saveItems();
+}
+
+
+function saveItems() { localStorage.setItem('items', JSON.stringify(itemsArr)); }
+function displayItems() {
+
+
+    itemsArr.length > 0 && addListerns()
+    exerciseList.innerHTML = itemsArr.map(item => ExerciseItem(item)).join('')
+}
+
+renderItems()
