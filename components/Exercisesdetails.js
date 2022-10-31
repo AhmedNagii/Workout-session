fa519347cf3be13e3caf1a8ea6e8f48958ca41b2
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-10-31 20:50:09 +0200

    handling api errors & custom user requests with inputs

--- components/Exercisesdetails.js
+++ components/Exercisesdetails.js
@@ -1,14 +1,12 @@
 export default function Exercisesdetails(item) {
 
 
-    const { difficulty, equipment, instructions, muscle, name, type } = item
+    const { equipment, instructions, name, type } = item
     return ` <li class="exercises-list__item">
-                <div class="items-container raw">
-                    <p class="item__text">${difficulty}</p>
-                    <p class="item__text">${equipment}</p>
-                    <p class="item__text">${muscle}</p>
-                    <p class="item__text">${name}</p>
-                    <p class="item__text">${type}</p>
+                <div class="items-container raw">                   
+                    <p class="item__text">Equipment: ${equipment}</p>
+                    <p class="item__text">Name: ${name}</p>
+                    <p class="item__text">Type: ${type}</p>
                 </div>
                 <p class="item__instructions">${instructions}</p>
             </li>`
--- exercises.js
+++ exercises.js
@@ -1,8 +1,11 @@
 
 import Exercisesdetails from "./components/exercisesdetails";
 
+const URL = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises'
 const exercisesList = document.querySelector('.exercises-list')
-
+const difficultySelector = document.getElementById('difficulty-selector')
+const muscleSelector = document.getElementById('muscle-selector')
+const getBtn = document.getElementById('get-exerciese-btn')
 const options = {
     method: 'GET',
     headers: {
@@ -12,13 +15,31 @@ const options = {
 };
 
 
-fetch('https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises', options)
-    .then(res => res.json()).then(data => {
-        exercisesList.innerHTML = data.map(item => {
-            return Exercisesdetails(item)
+getBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    const fullUrl = `${URL}?difficulty=${difficultySelector.value}&muscle=${muscleSelector.value}`
+    getApiData(fullUrl, options)
+})
+
+
+
+async function getApiData(fullUrl, options) {
+
+    try {
+        const response = await fetch(fullUrl, options)
+        if (response.status >= 200 && response.status <= 299) {
+            const data = await response.json()
+            exercisesList.innerHTML =
+                data.map(item => Exercisesdetails(item)).join("")
+        } else {
+            throw Error(response.statusText)
         }
-        ).join("")
-    })
-
-
-
+    } catch (err) {
+        exercisesList.innerHTML = ` <li class="failure-message">
+    <img src="assets/error-icon.png" alt="connection error icon">
+                <h2>
+                There is an error occurred<br/>
+                Please try later Or check your internet connection </h2>
+            </li>`
+    }
+}
